c1f5efe9e5dc726f5b56a50891a1f3d2ae3abc0f
dannyvoid <danny@void.bz>
2025-07-11 14:59:12 -0500

    Create archive_order.user.js
    0.4

---
+++ archive_order.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Amazon – Archive Order
+// @version      0.4
+// @description  Re-Add Order Archiving back into Amazon.com
+// @match        https://www.amazon.com/gp/css/order-history*
+// @grant        none
+// @updateURL    https://github.com/dannyvoid/random-userscripts/raw/main/archive_order.user.js
+// @downloadURL  https://github.com/dannyvoid/random-userscripts/raw/main/archive_order.user.js
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function addArchive() {
+    document
+      .querySelectorAll('li.yohtmlc-order-level-connections')
+      .forEach(li => {
+        if (li.querySelector('.archive-order')) return;
+
+        const detailsLink = li.querySelector('a[href*="order-details"]');
+        if (!detailsLink) return;
+        const params = new URLSearchParams(detailsLink.search);
+        const orderId = params.get('orderID') || params.get('orderId');
+        if (!orderId) return;
+
+        const sep = document.createElement('i');
+        sep.className = 'a-icon a-icon-text-separator';
+        sep.setAttribute('role', 'img');
+
+        const a = document.createElement('a');
+        a.className = 'a-link-normal archive-order';
+        a.href = `https://www.amazon.com/gp/css/order-history/archive/archiveModal.html?orderId=${orderId}`;
+        a.textContent = 'Archive';
+        a.target = '_blank';
+
+        li.appendChild(sep);
+        li.appendChild(a);
+      });
+  }
+
+  window.addEventListener('load', addArchive);
+  new MutationObserver(addArchive).observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+})();
